436e475f9960bc6d56365bebd49bdeeccab72294
Arthur Santos <56595301+santos-arthur@users.noreply.github.com>
2023-01-24 15:48:45 -0300

    feat: Code Styles Configs

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
-import { server } from "./server/server";
+import { server } from './server/server';
 
 server.listen(3333, () => {
-    console.log('App Rodando');
+  console.log('App Rodando');
 });
