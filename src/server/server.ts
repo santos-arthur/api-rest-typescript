294e7d661868cbded3d1ff005039cdb72e8f8644
Arthur Santos <56595301+santos-arthur@users.noreply.github.com>
2023-01-24 15:54:06 -0300

    fix: Fixing server.ts style

--- src/server/server.ts
+++ src/server/server.ts
@@ -1,9 +1,9 @@
-import express from "express";
+import express from 'express';
 
 const server = express();
 
 server.get('/', (req, res) => {
-    return res.send("Olá, Mundo!");
+  return res.send('Olá, Mundo!');
 });
 
 export { server };
